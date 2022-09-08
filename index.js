cfbed425865d83ce222511324037ee80f13b11dc
renanbdsantos@gmail.com <renanbdsantos@gmail.com>
2022-09-08 16:15:23 -0300

    Exercício Quadro de Saúde

---
+++ index.js
@@ -0,0 +1,11 @@
+var nome, peso, altura, dataInternaçao, diabetico
+
+nome = prompt("Insira seu nome: ");
+peso = prompt("Insira seu peso: ");
+altura = prompt("Insira sua altura: ");
+dataInternaçao = prompt("Insira a data de internação: ")
+diabetico = prompt ("Você é diabético? Responda Sim ou Não: ")
+
+alert ("Nome: " + nome + "\nPeso: " + peso + "\nAltura: " + altura + 
+"\nData de Internação: " + dataInternaçao + "\nDiabético: " + diabetico)
+
