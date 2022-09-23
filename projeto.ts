59f973c66e29eb892b84cfbee16b98acb13fdb5a
renanbdsantos@gmail.com <renanbdsantos@gmail.com>
2022-09-23 10:45:41 -0300

    extrato

---
+++ projeto.ts
@@ -0,0 +1,5 @@
+let extrato: string [] = []
+
+extrato.push("TRANSFERÊNCIA PIX SALGADO GIT INIT 23/09 - R$ 4,00", "DEPÓSITO 23/09  - R$ 50,00",
+"JUROS IOF 23/09 - R$ 4,55", "TRANFERÊNCIA CAJU 23/09 - R$ 500,00");
+console.log(extrato);
